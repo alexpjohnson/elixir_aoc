4ccde4d2c18f9c4af215004282ec94c7f1439ade
Alex <alexpjohnson@gmail.com>
2019-12-03 13:56:33 -0500

    Day 1 2019

---
+++ test/aoc/year_2019/day_01/the_tyranny_of_the_rocket_equation_test.exs
@@ -0,0 +1,28 @@
+defmodule Aoc.Year2019.Day01.TheTyrannyoftheRocketEquationTest do
+  use Aoc.DayCase
+  doctest Aoc.Year2019.Day01.TheTyrannyoftheRocketEquation, import: true
+
+  alias Aoc.Year2019.Day01.TheTyrannyoftheRocketEquation
+
+  describe "part_1/1" do
+    test "examples" do
+
+    end
+
+    @tag day: 01, year: 2019
+    test "input", %{input: input} do
+      assert input |> TheTyrannyoftheRocketEquation.part_1() == input
+    end
+  end
+
+  describe "part_2/1" do
+    test "examples" do
+
+    end
+
+    @tag day: 01, year: 2019
+    test "input", %{input: input} do
+      assert input |> TheTyrannyoftheRocketEquation.part_2() == input
+    end
+  end
+end
